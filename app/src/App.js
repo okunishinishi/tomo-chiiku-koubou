cea7ea872acb3f29ace5c40a78afb3ca6e6da9ad
Taka Okunishi <okunishinishi@gmail.com>
2023-12-14 09:07:39 +0900

    add top

--- app/src/App.js
+++ app/src/App.js
@@ -9,23 +9,29 @@ import './app.css'
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
 
-function App() {
+function App () {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
-      <div className="content">
-        <React.Suspense fallback={<em>Loading...</em>}>
-          <Router>
-            <Dynamic path="dynamic" />
-            <Routes path="*" />
-          </Router>
-        </React.Suspense>
-      </div>
+      <header>
+        <div>
+          <img src="./images/title.png" alt="とも知育工房"
+               className="header-img"
+          />
+        </div>
+      </header>
+      <main>
+        <div className="asided-container">
+
+          <article className="asided-main">
+            <React.Suspense fallback={<em>Loading...</em>}>
+              <Router>
+                <Dynamic path="dynamic"/>
+                <Routes path="*"/>
+              </Router>
+            </React.Suspense>
+          </article>
+        </div>
+      </main>
     </Root>
   )
 }
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Link } from '../components/Router'
 
 export default () => (
   <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
+    <h1>
+
+      <Link to="/blog">Blog</Link>
+
+    </h1>
   </div>
 )
