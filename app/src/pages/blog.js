de6a71be587d22449abb58e021bcbe49a5621763
Taka Okunishi <okunishinishi@gmail.com>
2023-12-18 21:05:32 +0900

    add list

--- app/src/pages/blog.js
+++ app/src/pages/blog.js
@@ -1,30 +1,13 @@
 import React from 'react'
 import { useRouteData } from 'react-static'
-//
-import { Link } from 'components/Router'
+import { List } from '../components/List'
 
-export default function Blog() {
+export default function Blog () {
   const { posts } = useRouteData()
   return (
     <div>
-      <h1>It's blog time.</h1>
-      <div>
-        <a href="#bottom" id="top">
-          Scroll to bottom!
-        </a>
-      </div>
-      <br />
-      All Posts:
-      <ul>
-        {posts.map(post => (
-          <li key={post.id}>
-            <Link to={`/blog/post/${post.id}/`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
-      <a href="#top" id="bottom">
-        Scroll to top!
-      </a>
+      全ての記事
+      <List posts={posts}/>
     </div>
   )
 }
